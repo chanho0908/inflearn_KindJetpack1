46bc1fdbc084fc3a4ac235387b5178c4db817187
chanho <uni070@naver.com>
2023-07-04 16:50:56 +0900

    인프런 친절한 Jetpack 개론

--- app/src/main/java/com/inhatc/inflearn_kindjetpack/compose/EditTextActivity.kt
+++ app/src/main/java/com/inhatc/inflearn_kindjetpack/compose/EditTextActivity.kt
@@ -7,18 +7,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import com.inhatc.inflearn_kindjetpack.compose.ui.theme.Inflearn_KindJetPackTheme
 
 class EditTextActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,19 +28,19 @@ class EditTextActivity : ComponentActivity() {
                     mutableStateOf("")
                 }
 
-                Column(modifier = Modifier
-                    .fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    TextField(
-                        value = text, onValueChange = setValue
 
-                    )
-                    Button(onClick = { /*TODO*/ }) {
-                        Text("클릭!!")
+
+                    Column(modifier = Modifier
+                        .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text("클릭!!")
+                        }
                     }
-                }
+
             }
         }
     }
